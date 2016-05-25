a06102b860e4474713ec9db70b3b9d56c5d3363d
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2016-05-25 13:06:53 -0400

    Switching to use an angular frontend.

---
+++ wwwroot/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name patientRegistryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the patientRegistryApp
+ */
+angular.module('patientRegistryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ wwwroot/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name patientRegistryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the patientRegistryApp
+ */
+angular.module('patientRegistryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
